8eb79a2a67ca38d96b85d7db3e0a022f1b91283b
YeahAyat <ayatbalmagambet.ab@gmail.com>
2025-10-05 18:41:28 +0500

    feat(algorithm): implement Kadane's algorithm with position tracking

--- src/main/java/org/yeah/algorithms/KadaneAlgorithm.java
+++ src/main/java/org/yeah/algorithms/KadaneAlgorithm.java
@@ -12,9 +12,43 @@ public class KadaneAlgorithm {
             this.startIndex = start;
             this.endIndex = end;
         }
+
+        @Override
+        public String toString() {
+            return String.format("MaxSum: %d, Range: [%d, %d]",
+                    maxSum, startIndex, endIndex);
+        }
     }
 
     public MaxSubarrayResult findMaxSubarray(int[] nums) {
-        return new MaxSubarrayResult(0, 0, 0);
+        if (nums == null || nums.length == 0) {
+            throw new IllegalArgumentException("Input array cannot be null or empty");
+        }
+
+        if (nums.length == 1) {
+            return new MaxSubarrayResult(nums[0], 0, 0);
+        }
+
+        int maxSoFar = nums[0];
+        int maxEndingHere = nums[0];
+        int start = 0, end = 0;
+        int tempStart = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (maxEndingHere < 0) {
+                maxEndingHere = nums[i];
+                tempStart = i;
+            } else {
+                maxEndingHere += nums[i];
+            }
+
+            if (maxEndingHere > maxSoFar) {
+                maxSoFar = maxEndingHere;
+                start = tempStart;
+                end = i;
+            }
+        }
+
+        return new MaxSubarrayResult(maxSoFar, start, end);
     }
 }
