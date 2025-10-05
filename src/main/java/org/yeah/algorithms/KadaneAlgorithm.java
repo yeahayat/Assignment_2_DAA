3275e9bff3126b9da90935da27ddda6d690b0123
YeahAyat <ayatbalmagambet.ab@gmail.com>
2025-10-05 18:51:13 +0500

    feat(metrics): add performance tracking (comparisons, array accesses)

--- src/main/java/org/yeah/algorithms/KadaneAlgorithm.java
+++ src/main/java/org/yeah/algorithms/KadaneAlgorithm.java
@@ -1,32 +1,39 @@
 package org.yeah.algorithms;
 
 public class KadaneAlgorithm {
+    private int comparisons;
+    private int arrayAccesses;
 
     public static class MaxSubarrayResult {
         public final int maxSum;
         public final int startIndex;
         public final int endIndex;
+        public final int operations;
 
-        public MaxSubarrayResult(int maxSum, int start, int end) {
+        public MaxSubarrayResult(int maxSum, int start, int end, int ops) {
             this.maxSum = maxSum;
             this.startIndex = start;
             this.endIndex = end;
+            this.operations = ops;
         }
 
         @Override
         public String toString() {
-            return String.format("MaxSum: %d, Range: [%d, %d]",
-                    maxSum, startIndex, endIndex);
+            return String.format("MaxSum: %d, Range: [%d, %d], Operations: %d",
+                    maxSum, startIndex, endIndex, operations);
         }
     }
 
     public MaxSubarrayResult findMaxSubarray(int[] nums) {
+        resetMetrics();
+
         if (nums == null || nums.length == 0) {
             throw new IllegalArgumentException("Input array cannot be null or empty");
         }
 
         if (nums.length == 1) {
-            return new MaxSubarrayResult(nums[0], 0, 0);
+            arrayAccesses++;
+            return new MaxSubarrayResult(nums[0], 0, 0, getTotalOperations());
         }
 
         int maxSoFar = nums[0];
@@ -34,14 +41,22 @@ public class KadaneAlgorithm {
         int start = 0, end = 0;
         int tempStart = 0;
 
+        arrayAccesses += 2;
+
         for (int i = 1; i < nums.length; i++) {
+            arrayAccesses++;
+            comparisons++;
+
             if (maxEndingHere < 0) {
                 maxEndingHere = nums[i];
                 tempStart = i;
+                arrayAccesses++;
             } else {
                 maxEndingHere += nums[i];
+                arrayAccesses++;
             }
 
+            comparisons++;
             if (maxEndingHere > maxSoFar) {
                 maxSoFar = maxEndingHere;
                 start = tempStart;
@@ -49,6 +64,18 @@ public class KadaneAlgorithm {
             }
         }
 
-        return new MaxSubarrayResult(maxSoFar, start, end);
+        return new MaxSubarrayResult(maxSoFar, start, end, getTotalOperations());
     }
+
+    private void resetMetrics() {
+        comparisons = 0;
+        arrayAccesses = 0;
+    }
+
+    private int getTotalOperations() {
+        return comparisons + arrayAccesses;
+    }
+
+    public int getComparisons() { return comparisons; }
+    public int getArrayAccesses() { return arrayAccesses; }
 }
